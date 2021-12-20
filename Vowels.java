f5d843743bfbee6e349e59f6559f47853956e190
vaibhavDharmik1 <vaibhavdharmik55.com>
2021-12-19 18:16:52 -1100

    Program To Find Vowels

---
+++ Vowels.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Vowels
+ 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Charcter");
+		char ch = sc.next().charAt(0);
+
+		if(ch == 'a' || ch == 'A')
+			System.out.println(ch+" is a vowel");
+		else if(ch == 'e' || ch == 'E')
+			System.out.println(ch+" is a vowel");
+		else if(ch == 'i' || ch == 'I')
+			System.out.println(ch+" is a vowel");
+		else if(ch == 'o' || ch == 'O')
+			System.out.println(ch+" is a vowel");
+		else if(ch == 'u' || ch == 'U')
+			System.out.println(ch+" is a vowel");
+		else 
+			System.out.println(ch+" is not a vowel");
+	}
+}
